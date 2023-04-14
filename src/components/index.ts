2c7e540713dffd81404c28c7f9f072a3e80ce5f6
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-14 10:45:56 -0800

    Add files via upload

---
+++ src/components/index.ts
@@ -0,0 +1,87 @@
+import IdVerification from "./IdentificationSteps/IdVerification";
+import SubmitDocument from "./IdentificationSteps/SubmitDocument";
+import PhotoInstructions from "./IdentificationSteps/PhotoInstructions";
+import UploadImage from "./IdentificationSteps/UploadImage";
+import SuccessMessage from "./IdentificationSteps/SuccessMessage";
+import Building from "./Building";
+import PreApp from "./PreApp/PreApp";
+import Loading from "./Loading";
+import CustomInput from "./CustomInput/CustomInput";
+import DateOfBirth from "./DetailsSteps/DateOfBirth";
+import Name from "./DetailsSteps/Name";
+import Gender from "./DetailsSteps/Gender";
+import Password from "./DetailsSteps/Password";
+import Avatar from "./DetailsSteps/Avatar";
+import Address from "./Onboarding/Address";
+import EmploymentStatus from "./Onboarding/EmploymentStatus";
+import FinancialStatus from "./Onboarding/FinancialStatus";
+import InvestmentStatus from "./Onboarding/InvestmentStatus";
+import MaritalStatus from "./Onboarding/MaritalStatus";
+import Nationality from "./Onboarding/Nationality";
+import NavBar from "./Navbar";
+import Logo from "./Logo/Logo";
+import Categories from "./Categories/Categories";
+import StockAdvisor from "./StockAdvisor/StockAdvisor";
+import Card from "./Card";
+import TopMovers from "./TopMovers/TopMovers";
+import DataLink from "./DataLink/DataLink";
+import StockOfTheMonth from "./StockOfTheMonth/StockOfTheMonth";
+import Stocks from "./Stocks/Stocks";
+import Stock from "./Stock/Stock";
+import ModalGroup from "./ModalGroup/ModalGroup";
+import ListStock from "./ListStock/ListStock";
+import ListStockCategory from "./ListStockCategory/ListStockCategory";
+import ListStockDescription from "./ListStockDescription/ListStockDescription";
+import ListStockDocuments from "./ListStockDocuments/ListStockDocuments";
+import ListStockImages from "./ListStockImages/ListStockImages";
+import ListStockDetails from "./ListStockDetails/ListStockDetails";
+import ListStockReview from "./ListStockReview/ListStockReview";
+import News from "./News/News";
+import Calculator from "./Calculator/Calculator";
+import PerformanceCard from "./PerformanceCard/PerformanceCard";
+import InsightsCard from "./InsightsCard";
+
+export {
+    IdVerification,
+    SubmitDocument,
+    PhotoInstructions,
+    UploadImage,
+    Loading,
+    Building,
+    PreApp,
+    CustomInput,
+    Name,
+    Gender,
+    DateOfBirth,
+    Password,
+    Avatar,
+    SuccessMessage,
+    Address,
+    EmploymentStatus,
+    FinancialStatus,
+    InvestmentStatus,
+    MaritalStatus,
+    Nationality,
+    NavBar,
+    Logo,
+    Categories,
+    StockAdvisor,
+    Card,
+    TopMovers,
+    DataLink,
+    StockOfTheMonth,
+    Stocks,
+    Stock,
+    ModalGroup,
+    ListStock,
+    ListStockCategory,
+    ListStockDocuments,
+    ListStockDescription,
+    ListStockImages,
+    ListStockDetails,
+    ListStockReview,
+    Calculator,
+    News,
+    PerformanceCard,
+    InsightsCard
+}
