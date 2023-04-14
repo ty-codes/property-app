40971298ec57679dd344eb4c93cf4bca510b6572
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-14 11:04:28 -0800

    Add files via upload

---
+++ src/pages/VerifyIdentity/VerifyIdentity.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { IdVerification, SuccessMessage, SubmitDocument, PhotoInstructions, UploadImage } from "../../components";
+import "./VerifyIdentity.css";
+import { Logo } from "../../assets/svg";
+
+
+export default function VerifyIdentity(): JSX.Element {
+    
+
+  const [step, setStep] = useState<number>(0);
+  const [documentType, setDocumentType] = useState("");
+
+  function renderPage() {
+		switch (step) {
+			case 0:
+				return <IdVerification setStep={setStep} />;
+			case 1:
+				return <SubmitDocument setStep={setStep} setDocumentType={setDocumentType} />;
+			case 2:
+				return <PhotoInstructions setStep={setStep} />;
+			case 3:
+				return <UploadImage setStep={setStep} />;
+			case 4:
+				return <SuccessMessage />;
+			default:
+				return <IdVerification setStep={setStep} />;
+		}
+	}
+   
+    return (
+      <div className="id-wrapper">
+        <div className="logo-wrap">
+        <Logo />
+        <span>Property</span>
+        </div>
+        {renderPage()}
+      </div>
+    )
+}
---
+++ src/pages/VerifyPhoneNumber/VerifyPhoneNumber.tsx
@@ -0,0 +1,71 @@
+import { useNavigate, useLocation, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import "./VerifyPhoneNumber.css";
+import { Building } from "../../components";
+
+const VerifyPhoneNumber = (): JSX.Element => {
+    const [code, setCode] = useState<string[]>([]);
+    const navigate = useNavigate();
+    const location = useLocation().search;
+    const searchParams = new URLSearchParams(location)
+    const phoneNumber = searchParams.get("phoneNumber");
+
+    const [disable, setDisabled] = useState(true);
+
+    console.log(phoneNumber);
+    useEffect(() => {
+        setDisabled(false);
+    }, [code.length === 4]);
+
+    const censorText = (text: string | null) => {
+        if (text) {
+            if (text === phoneNumber) {
+                var result = "";
+                for (var i = 0; i < 4; i++) result += "*"; return phoneNumber.slice(0, 7) + result;
+            } else {
+                return;
+            }
+        }
+    }
+
+    const verifyCode = (e: React.FormEvent<HTMLFormElement>): void => {
+        e.preventDefault();
+        // console.log(code)
+        // send code to backend
+        // reroute to login
+        navigate("/signin")
+
+    }
+
+    return (
+        <div id="registerpage" className="pagewrapper">
+            <Building />
+
+            <div className="page">
+                <div>
+                    <h2>OTP verificaion</h2>
+                    <p>Check your inbox. We've sent you the code
+                        at {censorText(phoneNumber)}
+                    </p>
+                    <form onSubmit={(e) => { verifyCode(e) }}>
+                        <div className="code">
+                            <input type='text' maxLength={1} onChange={(e: React.ChangeEvent<HTMLInputElement>) => { code[0] = e.target.value; setCode(code); }} />
+                            <input type='text' maxLength={1} onChange={(e: React.ChangeEvent<HTMLInputElement>) => { code[1] = e.target.value; setCode(code) }} />
+                            <input type='text' maxLength={1} onChange={(e: React.ChangeEvent<HTMLInputElement>) => { code[2] = e.target.value; setCode(code) }} />
+                            <input type='text' maxLength={1} onChange={(e: React.ChangeEvent<HTMLInputElement>) => { code[3] = e.target.value; setCode(code) }} />
+
+                        </div>
+                        <span className="request">Didn't receive any code?
+                            <Link className="link" to='/'>  Request again</Link>
+                        </span><br />
+                        <button style={{ marginTop: "1em" }} disabled={disable}
+                            type="submit" className='btn filled_btn'>Verify</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default VerifyPhoneNumber;
