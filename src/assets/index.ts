6c6394c55affce03ff3be5b288f88a8686edd419
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-14 10:56:57 -0800

    Add files via upload

---
+++ src/assets/index.ts
@@ -0,0 +1,47 @@
+import building from "./building.png";
+import check from "./check.png";
+import appleIcon from "./apple.png";
+import googleIcon from "./google.png";
+import facebookIcon from "./fb.png"
+import partyhatIcon from "./partyhat.png";
+import hero from "./hero.png";
+import logo from "./logo.png";
+import mail from "./mail.png";
+import successCheckmark from "./success-checkmark.png";
+import bank from "./Bank.png"
+import elysium from "./elysium.png";
+import pig from "./pig.png";
+import metrics from "./metrics.png";
+import bag from "./bag.png";
+import briefcase from "./briefcase.png";
+import cardBackground from "./card-bg.png";
+import graph from "./graph.png";
+import dangote from "./dangote.png";
+import gtb from "./gtb.png";
+import fbn from "./fbn.png";
+import oando from "./oando.png";
+
+export {
+    building,
+    check,
+    appleIcon,
+    googleIcon,
+    facebookIcon,
+    partyhatIcon,
+    hero,
+    logo,
+    mail,
+    successCheckmark,
+    bank,
+    elysium,
+    pig,
+    metrics,
+    bag,
+    briefcase,
+    cardBackground,
+    graph,
+    dangote,
+    gtb,
+    fbn,
+    oando
+}
