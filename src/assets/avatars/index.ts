48cff9f36a68d8a43f0f5e9e1152793e937e48db
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-14 10:50:03 -0800

    Add files via upload

---
+++ src/assets/avatars/index.ts
@@ -0,0 +1,19 @@
+import Avatar1 from "./avatar.png"
+import Avatar2 from "./avatar2.png"
+import Avatar3 from "./avatar3.png"
+import Avatar4 from "./avatar4.png"
+import Avatar5 from "./avatar5.png"
+import Avatar6 from "./avatar6.png"
+import Avatar7 from "./avatar7.png"
+import Avatar8 from "./avatar8.png"
+
+export {
+    Avatar1,
+    Avatar2,
+    Avatar3,
+    Avatar4,
+    Avatar5,
+    Avatar6,
+    Avatar7,
+    Avatar8
+}
