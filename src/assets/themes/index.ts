7bfc4868c0c710cdeb236646367846aa1ff64985
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-14 10:57:07 -0800

    Add files via upload

---
+++ src/assets/themes/index.ts
@@ -0,0 +1,71 @@
+import bag from "./bag.png";
+import bankBlackAndWhite from "./bank-bw.png";
+import bankCream from "./bank-cream.png";
+import bank from "./Bank.png";
+import bill from "./bill.png";
+import book from "./book.png";
+import bonds from "./bonds.png";
+import briefcase from "./brief-case.png";
+import camera from "./camera.png";
+import card from "./card.png";
+import cart from "./cart.png";
+import cash from "./cash.png";
+import cup from "./cup.png";
+import dollarCoin from "./dollar-coin.png";
+import dropper from "./dropper.png";
+import elysium from "./elysium.png";
+import etfCoin from "./etf-coin.png";
+import euroCoin from "./euro-coin.png";
+import folder from "./folder.png";
+import hat from "./hat.png";
+import healthCheck from "./health-check.png";
+import locks from "./locks.png";
+import metrics from "./metrics.png";
+import pawn from "./pawn.png";
+import pig from "./pig.png";
+import purse from "./purse.png";
+import rocket from "./rocket.png";
+import shield from "./shield.png";
+import slate from "./slate.png";
+import suitcase from "./suitcase.png";
+import wallet from "./wallet.png";
+import warren from "./warren.png";
+import wheel from "./wheel.png";
+
+
+export {
+    bag,
+    bankBlackAndWhite,
+    bank,
+    bankCream,
+    bill,
+    book,
+    bonds,
+    briefcase,
+    camera,
+    card,
+    cart,
+    cash,
+    cup,
+    dollarCoin,
+    dropper,
+    elysium,
+    etfCoin,
+    euroCoin,
+    folder,
+    hat,
+    healthCheck,
+    locks,
+    metrics,
+    pawn,
+    pig,
+    purse,
+    rocket,
+    shield,
+    slate,
+    suitcase,
+    wallet,
+    warren,
+    wheel
+
+}
