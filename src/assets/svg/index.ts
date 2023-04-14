c8f64a2694cbea57376e84f47cb914cd64be76f1
Bolatito Adeoye <titoadeoye00@gmail.com>
2023-04-14 10:52:26 -0800

    Add files via upload

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,24 @@
+import {ReactComponent as Logo} from "./logo.svg";
+import {ReactComponent as FrameIcon} from "./frame.svg";
+import {ReactComponent as IdentityIcon} from "./identity.svg";
+import {ReactComponent as LooperIcon} from "./looper.svg";
+import {ReactComponent as TicKIcon} from "./tick.svg";
+import {ReactComponent as SpiralIcon} from "./spiral.svg";
+import {ReactComponent as AdvisorLogo} from "./advisor-logo.svg";
+import {ReactComponent as StatDownIcon} from "./stat-down.svg";
+import {ReactComponent as FastArrowDown} from "./fast-arrow-down.svg";
+import {ReactComponent as CloseIcon} from "./close.svg";
+
+
+export {
+    Logo,
+    FrameIcon,
+     IdentityIcon,
+     LooperIcon,
+     TicKIcon,
+     SpiralIcon,
+     AdvisorLogo,
+     StatDownIcon,
+     FastArrowDown,
+     CloseIcon
+}
